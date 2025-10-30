3d4f327693255e524afb507b34503c4981594ecb
Darya-Gronova <d.koroleva1001@gmail.com>
2025-10-30 14:00:14 +0300

    Интеграция Pytest & Playwright

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,34 +1,44 @@
-import pytest
-from playwright.sync_api import expect, Page
+from playwright.sync_api import sync_playwright, expect
 
 
-def test_successful_authorization(chromium_page: Page):  # Создаем тестовую функцию
+def test_successful_authorization():
 
-    # Переходим на страницу входа
-    chromium_page.goto("https://devatlaskm.marketing-logic.ru/authorization")
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        context = browser.new_context()
+        page = context.new_page()
 
-    # Заполняем поле логин
-    login_input = page.get_by_test_id('login-input').locator('input')
-    login_input.fill("apetrovikh")
+        # Переходим на страницу входа
+        page.goto("https://devatlaskm.marketing-logic.ru/authorization")
 
-    # Проверяем, что в поле находится ожидаемое значение
-    expect( login_input).to_have_value("apetrovikh")
+        # Заполняем поле логин
+        login_input = page.get_by_test_id('login-input')
+        login_input.fill("dariak")
 
-    # Заполняем поле пароль
-    password_input = chromium_page.locator('//*[@id="root"]/div/div/div/div/div/input[2]')
-    password_input.fill("2S9u7m2d5s")
+        # Проверяем, что в поле находится ожидаемое значение
+        expect(login_input).to_have_value("dariak")
 
-    # Проверяем, что в поле находится ожидаемое значение
-    expect(password_input).to_have_value("2S9u7m2d5s")
+        # Заполняем поле пароль
+        password_input = page.get_by_test_id('password-input')
+        password_input.fill("r63l80AV")
 
-    # Нажимаем на кнопку Login
-    login_button = chromium_page.locator('//*[@id="root"]/div/div/div/div/div/button')
-    login_button.click()
+        # Проверяем, что в поле находится ожидаемое значение
+        expect(password_input).to_have_value("r63l80AV")
 
-    # Проверяем авторизацию по меню
-    navigation = chromium_page.locator('//*[@id="root"]/div/div/div/header/div[2]/div[1]/ul')
+        # Нажимаем на кнопку Login
+        login_button = page.get_by_test_id('login-button')
+        login_button.click()
 
-    # Проверяем, что каждый текст есть внутри блока
-    expect(navigation).to_contain_text("Мониторинг")
-    expect(navigation).to_contain_text("Объекты")
-    expect(navigation).to_contain_text("Справочники")
+        context.storage_state(path="browser-state.json")
+
+        # Проверяем авторизацию по меню
+        navigation = page.locator('//*[@id="root"]/div/div/div/header/div[2]/div[1]/ul')
+
+        # Проверяем, что каждый текст есть внутри блока
+        expect(navigation).to_contain_text("Мониторинг")
+        expect(navigation).to_contain_text("Объекты")
+        expect(navigation).to_contain_text("Справочники")
+
+def test_failed_authorization():
+    with sync_playwright() as p:
+        ...
