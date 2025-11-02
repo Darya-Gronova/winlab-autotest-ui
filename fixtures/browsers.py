6ebd0edf4c53e77c66b6e945a4458fef2a7df545
Darya-Gronova <d.koroleva1001@gmail.com>
2025-11-02 16:48:01 +0300

    plagins

---
+++ fixtures/browsers.py
@@ -0,0 +1,16 @@
+import pytest
+from playwright.sync_api import Page, Playwright
+
+
+@pytest.fixture
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=True)
+    yield browser.new_page()
+    browser.close()
+
+@pytest.fixture
+def chromium_page_with_authorization(initialize_browser_state, playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=True)
+    context = browser.new_context(storage_state="browser_state.json")
+    yield context.new_page()
+    browser.close()
